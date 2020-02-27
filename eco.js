632fc03c275eb856129224e35b6e392b3e7e2cda
teovonkesselstatt <teovonkesselstatt@gmail.com>
2020-02-27 15:47:57 -0300

    deleted templates folder

---
+++ eco.js
@@ -0,0 +1,29 @@
+document.querySelector("#commie").onclick = function() {
+    document.querySelector('body').style.backgroundColor = "red";
+    document.querySelector('.header').style.backgroundColor = "red";
+    document.querySelector('#eco').style = "color:white;";
+    document.querySelector('.subsection').style = "color:white;";
+    document.querySelector('p').style = "color:white;";
+    document.querySelector('.extra_margin').style = "color:white;";
+    document.querySelector('#eco').innerHTML = "Commie";
+}
+
+document.querySelector("#lib").onclick = function() {
+    document.querySelector('body').style.backgroundColor = "lightblue";
+    document.querySelector('.header').style.backgroundColor = "blue";
+    document.querySelector('p').style = "color:black;";
+    document.querySelector('#eco').style = "color:black;";
+    document.querySelector('.extra_margin').style = "color:black;";
+    document.querySelector('.subsection').style = "color:blue;";
+    document.querySelector('#eco').innerHTML = "Monetarist/liberal :)";
+}
+
+document.querySelector("#green").onclick = function() {
+    document.querySelector('body').style.backgroundColor = "green";
+    document.querySelector('.header').style.backgroundColor = "green";
+    document.querySelector('p').style = "color:white;";
+    document.querySelector('#eco').style = "color:white;";
+    document.querySelector('.extra_margin').style = "color:white;";
+    document.querySelector('.subsection').style = "color:white;";
+    document.querySelector('#eco').innerHTML = "Progressist/SJW";
+}
