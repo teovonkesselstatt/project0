98ede14f364591fcb72a2c32c27e1fb8e7e125c8
teovonkesselstatt <teovonkesselstatt@gmail.com>
2020-03-02 20:12:07 -0300

    corrected eco page

--- eco.js
+++ eco.js
@@ -5,6 +5,8 @@ document.querySelector("#commie").onclick = function() {
     document.querySelector('.subsection').style = "color:white;";
     document.querySelector('p').style = "color:white;";
     document.querySelector('.extra_margin').style = "color:white;";
+    document.querySelector('#div').style = "color:white;";
+    document.querySelector('#lal').style = "color:white;";
     document.querySelector('#eco').innerHTML = "Commie";
 }
 
@@ -14,6 +16,8 @@ document.querySelector("#lib").onclick = function() {
     document.querySelector('p').style = "color:black;";
     document.querySelector('#eco').style = "color:black;";
     document.querySelector('.extra_margin').style = "color:black;";
+    document.querySelector('#div').style = "color:black;";
+    document.querySelector('#lal').style = "color:black;";
     document.querySelector('.subsection').style = "color:blue;";
     document.querySelector('#eco').innerHTML = "Monetarist/liberal :)";
 }
@@ -24,6 +28,8 @@ document.querySelector("#green").onclick = function() {
     document.querySelector('p').style = "color:white;";
     document.querySelector('#eco').style = "color:white;";
     document.querySelector('.extra_margin').style = "color:white;";
+    document.querySelector('#div').style = "color:white;";
+    document.querySelector('#lal').style = "color:white;";
     document.querySelector('.subsection').style = "color:white;";
     document.querySelector('#eco').innerHTML = "Progressist/SJW";
 }
